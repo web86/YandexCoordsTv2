4c7777ad16919db7b1be222e144ebab949dc1a97
Konstantin Shulyarenko <support@web86.ru>
2025-12-22 10:43:49 +0300

    Rename YandexCoordsTvInputRender class to YandexCoordsTvInputRender2

--- core/components/myyandexcoordstv/tv/input/yandexcoordstv.class.php
+++ core/components/myyandexcoordstv/tv/input/yandexcoordstv.class.php
@@ -1,13 +1,13 @@
 <?php
 
-if(!class_exists('YandexCoordsTvInputRender')) {
-    class YandexCoordsTvInputRender extends modTemplateVarInputRender {
+if(!class_exists('YandexCoordsTvInputRender2')) {
+    class YandexCoordsTvInputRender2 extends modTemplateVarInputRender {
         public function getTemplate() {
-            return $this->modx->getOption('core_path').'components/yandexcoordstv/tv/tpl/yandexcoordstv.tpl';
+            return $this->modx->getOption('core_path').'components/yandexcoordstv/tv/tpl/myyandexcoordstv.tpl';
         }
         public function process($value,array $params = array()) {
            
         }
     }
 }
-return 'YandexCoordsTvInputRender';
+return 'YandexCoordsTvInputRender2';

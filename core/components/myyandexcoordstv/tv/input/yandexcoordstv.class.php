d9810b4c02772ade11a8915f56633cdf13c805b7
Konstantin Shulyarenko <support@web86.ru>
2025-12-22 10:47:22 +0300

    Fix template path in YandexCoordsTvInputRender2

--- core/components/myyandexcoordstv/tv/input/yandexcoordstv.class.php
+++ core/components/myyandexcoordstv/tv/input/yandexcoordstv.class.php
@@ -3,7 +3,7 @@
 if(!class_exists('YandexCoordsTvInputRender2')) {
     class YandexCoordsTvInputRender2 extends modTemplateVarInputRender {
         public function getTemplate() {
-            return $this->modx->getOption('core_path').'components/yandexcoordstv/tv/tpl/myyandexcoordstv.tpl';
+            return $this->modx->getOption('core_path').'components/myyandexcoordstv/tv/tpl/myyandexcoordstv.tpl';
         }
         public function process($value,array $params = array()) {
            

46b6f9f1cc44dd7b3912751086231db9f20bc225
Konstantin Shulyarenko <support@web86.ru>
2025-12-22 09:38:31 +0300

    Change siteBuilder instance name to 'myyandexcoordstv'

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -3,5 +3,5 @@ require_once 'build.class.php';
 $resolvers = array(
   'settings'
 );
-$builder = new siteBuilder('yandexcoordstv', '1.0.2', 'beta', $resolvers);
+$builder = new siteBuilder('myyandexcoordstv', '1.0.2', 'beta', $resolvers);
 $builder->build();
